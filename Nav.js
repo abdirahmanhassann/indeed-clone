b24cb28fd1b2c5d105f12b1c607d158bb927e045
abdirahmanhassann <113362995+abdirahmanhassann@users.noreply.github.com>
2022-11-07 18:45:38 +0300

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import logo from './indeelogo.jpg';
+import {FaUserAlt} from 'react-icons/fa' 
+import {FaAlignJustify} from  'react-icons/fa' 
  const Nav=()=>{
 return(
     <>
     <nav>
+    <img src={logo} className="indeedlogo"></img>
     <span className='navspan1'>
-    <a className='a1'>img</a>
     <a className='a1'>Find jobs</a>
     <a className='a1'>Company reviews</a>
     <a className='a1'>Salary guide</a>
@@ -15,6 +17,12 @@ return(
 <a className='a21'>Sign in</a>
     </span>
     <a className='a2'>Employers / Post job</a>
+    
+        <div className="smallnav">
+    <a className="usericon"><FaUserAlt/> Signin</a>
+    <a ><FaAlignJustify className="hamburger"/></a>
+    </div>
+   
    </nav>
     </>
 )
